d94c782c46d7187b4a08ad3539eb3dc49008e770
javohirturgunboyev <javohirboy009@gmail.com>
2024-05-20 22:52:40 +0500

    tugadi oxirgi 3tasiga ulgurmadim

---
+++ index.js
@@ -0,0 +1,179 @@
+// 1-masala
+// let a =+prompt 5 ;
+// if (a>0) {
+//     a++;
+// }
+
+// console.log(a);
+
+// 2-masala
+// let b =+prompt 5;
+// if (a>0) {
+//     a++;
+// } else{
+// a -= 2;
+// }
+
+// console.log(a)
+
+// 3-masala
+// let c = +prompt 5;
+// if (a>0) {
+//     a++;
+// } else{
+//     a -= 2;
+
+
+// } {
+// a = 10;
+// }
+
+// 4-masala
+// let m = +prompt ("1-sonni kiriting");
+// let n = +prompt ("2-sonni kiriting") ;
+// let o = +prompt ("3-sonni kiriting");
+// let num =0;
+
+// if(m>0){
+// num = num+1;
+// }
+
+// if(n>0){
+//     num = num+1;
+//     }
+
+//     if(o>0){
+//         num = num+1;
+//         }
+//  console.log(num);
+
+// 5-masala
+// let num1 = +prompt("1-sonni kiriting")
+// let num2 = +prompt("2-sonni kiriting")
+// let num3 = +prompt("3-sonni kiriting")
+// let musbat =0;
+
+// if(num1>0){
+// musbat++;
+// }
+
+// if(num2>0){
+//     musbat++;
+//     }
+    
+//     if(num3>0){
+//         musbat++;
+//         }
+
+//         console.log(musbat);
+
+// 6-masala
+// let sum1 = +prompt("1-sonni kiriting");
+// let sum2 = +prompt("2-sonni kiriting");
+// let max;
+
+// if(sum1>sum2){
+// max = sum1;
+// } else{
+// max = sum2;
+// }
+
+// console.log(max);
+
+// 7-masala
+// let sum1 = +prompt("1-sonni kiriting");
+// let sum2 = +prompt("2-sonni kiriting");
+// let max;
+// let min;
+// if(sum1>sum2){
+
+// min = sum2;
+// } else{
+
+// min = sum1;
+// }
+
+
+// console.log(min);
+
+// 8-masala
+
+// let sum1 = +prompt("1-sonni kiriting");
+// let sum2 = +prompt("2-sonni kiriting");
+// let max;
+// let min;
+// if(sum1>sum2){
+// max = sum1;
+// min = sum2;
+// } else{
+// max = sum2;
+// min = sum1;
+// }
+
+// console.log(max);
+// console.log(min);
+
+// 9-masala
+
+// let sum1 = +prompt("1-sonni kiriting");
+// let sum2 = +prompt("2-sonni kiriting");
+// let sum;
+
+// if (sum1>sum2){
+// sum = sum1;
+// sum1 = sum2;
+// sum2 = sum;
+// }
+
+// 10-masala
+
+// let sum1 = +prompt("1-sonni kiriting");
+// let sum2 = +prompt("2-sonni kiriting");
+
+// if (sum1==sum2) {
+//     sum1 = 0;
+//     sum2 = 0;
+// } else{
+// let c = sum1+sum2;
+// sum1 = c;
+// sum2 = c;
+// }
+
+// 11-masala
+
+// let sum1 = +prompt("1-sonni kiriting");
+// let sum2 = +prompt("2-sonni kiriting");
+
+// if (sum1==sum2) {
+//     sum1 = 0;
+//     sum2 = 0;
+//  } else{
+// let max;
+// if(sum1>sum2){
+// max = sum1;
+// } else{
+// max = sum2;
+// }
+// sum1 = max;
+// sum2 = max;
+
+// }
+
+// 12-masala
+
+// let sum1 = +prompt("1-sonni kiriting");
+// let sum2 = +prompt("2-sonni kiriting");
+// let sum3 = +prompt("3-sonni kiriting");
+
+// let min;
+
+// if (sum1<sum2) {
+//   min = sum1;  
+// } else{
+// min = sum2;
+// }
+
+// if (min>sum3) {
+//    min = sum3; 
+// }
+
